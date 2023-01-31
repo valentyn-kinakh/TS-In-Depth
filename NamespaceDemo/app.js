6daffa0c3c00297c55deab8c306f42e8b62900fd
valentyn-kinakh <valentyn_kinakh@epam.com>
2023-01-31 12:25:58 +0200

    add namespace folder

---
+++ NamespaceDemo/app.js
@@ -0,0 +1,6 @@
+/// <reference path="utility-functions.ts" />
+var result1 = Utility.maxBooksAllowed(15);
+console.log(result1);
+var util = Utility.Fees;
+var result2 = util.calculateLateFee(30);
+console.log(result2);
---
+++ NamespaceDemo/app.ts
@@ -0,0 +1,9 @@
+/// <reference path="utility-functions.ts" />
+
+const result1 = Utility.maxBooksAllowed(15);
+
+console.log(result1);
+
+import util = Utility.Fees;
+const result2 = util.calculateLateFee(30);
+console.log(result2);
