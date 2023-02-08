6bb9ea61ecc6ad9840b4db13a146d0ae4c75f2af
valentyn-kinakh <valentyn_kinakh@epam.com>
2023-02-08 08:59:15 +0200

    add Shelf2 class

--- src/classes/shelf.ts
+++ src/classes/shelf.ts
@@ -1,6 +1,26 @@
-import {ShelfItem} from '../interfaces';
+import {Book, Magazine, ShelfItem} from '../interfaces';
 
 export default class Shelf<T extends ShelfItem> {
+    private items: T[] = [];
+
+    add(item: T): void {
+        this.items.push(item);
+    };
+
+    getFirst(): T {
+        return this.items[0];
+    }
+
+    find(title: string): T {
+        return this.items.find(item => item.title === title);
+    }
+
+    printTitles(): void {
+        this.items.forEach(item => console.log(item.title));
+    }
+}
+
+export class Shelf2<T extends (Book | Magazine)> {
     private items: T[] = [];
 
     add(item: T): void {
